c8e2fedf785d0fa318569eca7e05bf410600ed0e
VinuJ <severematrixoverload@gmail.com>
2021-11-16 15:39:48 +0000

    Setting up Redux store

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import "./Home.scss";
+import MovieApi from "../../common/apis/MovieApi";
+import { APIkey } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText: string = "Harry";
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(
+        `?s=${movieText}&type=movie&apikey=${APIkey}`
+      ).catch((error) => {
+        console.log("Error: " + error);
+      });
+      console.log("API Response: " + response);
+    };
+    fetchMovies()
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./features/store";
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
