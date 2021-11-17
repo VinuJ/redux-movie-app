03b93fc538c52459d77708c570a3feace6f8a279
Vinu Jey <severematrixoverload@gmail.com>
2021-11-17 02:25:29 +0000

    Added shows listing

--- src/components/MovieListing/MovieListing.tsx
+++ src/components/MovieListing/MovieListing.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
-import "./MovieListing.scss"
+import "./MovieListing.scss";
 
-interface MovieData {
+interface Data {
   Poster: string;
   Title: string;
   Type: string;
@@ -14,28 +14,41 @@ interface MovieData {
 
 const MovieListing = () => {
   const movies: any = useSelector(getAllMovies);
+  const shows: any = useSelector(getAllShows);
   console.log(movies);
 
-  let renderMovies: string | JSX.Element = "";
+  let renderMovies,
+    renderShows: string | JSX.Element = "";
 
-  if (movies.Response === 'True') {
-    renderMovies = movies.Search.map((movie: MovieData, index: number) => {
-      return <MovieCard key={index} data={movie} />
-    })
+  if (movies.Response === "True") {
+    renderMovies = movies.Search.map((movie: Data, index: number) => {
+      return <MovieCard key={index} data={movie} />;
+    });
   } else {
     renderMovies = <div className="movies-error">{movies.Error}</div>;
+  }
+
+  if (shows.Response === "True") {
+    renderShows = shows.Search.map((show: Data, index: number) => {
+      return <MovieCard key={index} data={show} />;
+    });
+  } else {
+    renderShows = <div className="shows-error">{shows.Error}</div>;
   }
 
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">
-          {renderMovies}
-        </div>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default MovieListing;
--- src/features/movies/movieSlice.tsx
+++ src/features/movies/movieSlice.tsx
@@ -3,24 +3,37 @@ import MovieApi from "../../common/apis/MovieApi";
 import { APIkey } from "../../common/apis/MovieApiKey";
 import { RootState } from "../store";
 
-interface MovieState {
+interface State {
   movies: Object;
+  shows: Object;
 }
 
 const movieText: string = "Harry";
+const showText: string = "Cars";
 
 export const fetchAsyncMovies = createAsyncThunk(
   "movies/fetchAsyncMovies",
   async () => {
     const response = await MovieApi.get(
       `?s=${movieText}&type=movie&apikey=${APIkey}`
-    )
-    return response.data
+    );
+    return response.data;
   }
 );
 
-const initialState: MovieState = {
+export const fetchAsyncShows = createAsyncThunk(
+  "movies/fetchAsyncShows",
+  async () => {
+    const response = await MovieApi.get(
+      `?s=${showText}&type=series&apikey=${APIkey}`
+    );
+    return response.data;
+  }
+);
+
+const initialState: State = {
   movies: {},
+  shows: {},
 };
 
 const movieSlice = createSlice({
@@ -31,22 +44,29 @@ const movieSlice = createSlice({
       state.movies = payload;
     },
   },
+  // 'Builder callback' approach required for TypeScript to correctly infer types
   extraReducers: (builder) => {
     builder.addCase(fetchAsyncMovies.pending, () => {
-      console.log("Pending")
-    })
+      console.log("Pending");
+    });
 
-    builder.addCase(fetchAsyncMovies.fulfilled, (state: MovieState, { payload }) => {
-      console.log("Fetched Successfully")
-      return { ...state, movies: payload}
-    })
-    
+    builder.addCase(fetchAsyncMovies.fulfilled, (state: State, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, movies: payload };
+    });
+
     builder.addCase(fetchAsyncMovies.rejected, () => {
-      console.log("Rejected")
-    })
-  }
+      console.log("Rejected");
+    });
+
+    builder.addCase(fetchAsyncShows.fulfilled, (state: State, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, shows: payload };
+    });
+  },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state: RootState) => state.movies.movies;
+export const getAllShows = (state: RootState) => state.movies.shows;
 export default movieSlice.reducer;
