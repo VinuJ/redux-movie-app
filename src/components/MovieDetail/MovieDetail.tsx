e687818f043b0e31ab80a87a8c1655aa09632b35
Vinu Jey <severematrixoverload@gmail.com>
2021-11-18 17:59:31 +0000

    Added loading message for MovieDetails

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -1,76 +1,99 @@
 import React, { useEffect } from "react";
 import { Params, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAsyncDetails, getDetails } from "../../features/movies/movieSlice";
-import { faStar, faThumbsUp, faFilm, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import {
+  fetchAsyncDetails,
+  getDetails,
+  removeDetails,
+} from "../../features/movies/movieSlice";
+import {
+  faStar,
+  faThumbsUp,
+  faFilm,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./MovieDetail.scss"
+import "./MovieDetail.scss";
 
 const MovieDetail = () => {
-  const {imdbID} = useParams();
+  const { imdbID } = useParams();
   const dispatch = useDispatch();
-  const data = useSelector(getDetails)
-  console.log(data)
+  const data = useSelector(getDetails);
+  console.log(data);
   useEffect(() => {
-    dispatch(fetchAsyncDetails(imdbID))
-  }, [dispatch, imdbID])
+    dispatch(fetchAsyncDetails(imdbID));
+
+    return () => {
+      dispatch(removeDetails());
+    };
+  }, [dispatch, imdbID]);
   return (
     <div className="movie-section">
-      <div className="left-section">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-stats">
-          <span>
-            IMDB Rating <FontAwesomeIcon id='star' icon={faStar} /> : {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <FontAwesomeIcon id='thumbs-up' icon={faThumbsUp} /> : {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <FontAwesomeIcon id='film' icon={faFilm} /> : {data.Runtime}
-          </span>
-          <span>
-            Year <FontAwesomeIcon id='calendar' icon={faCalendar} /> : {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <data className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>Fetching...</div>
+      ) : (
+        <>
+          <div className="left-section">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-stats">
+              <span>
+                IMDB Rating <FontAwesomeIcon id="star" icon={faStar} /> :{" "}
+                {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <FontAwesomeIcon id="thumbs-up" icon={faThumbsUp} />{" "}
+                : {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <FontAwesomeIcon id="film" icon={faFilm} /> :{" "}
+                {data.Runtime}
+              </span>
+              <span>
+                Year <FontAwesomeIcon id="calendar" icon={faCalendar} /> :{" "}
+                {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+            </div>
+            <div className="movie-info">
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+            </div>
+            <div className="movie-info">
+              <div>
+                <span>Genres</span>
+                <span>{data.Genre}</span>
+              </div>
+            </div>
+            <div className="movie-info">
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+            </div>
+            <div className="movie-info">
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-        </data>
-        <data className="movie-info">
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="right-section">
+            <div className="movie-image">
+              <img src={data.Poster} alt={data.Title} />
+            </div>
           </div>
-        </data>
-        <data className="movie-info">
-          <div>
-            <span>Genres</span>
-            <span>{data.Genre}</span>
-          </div>
-        </data>
-        <data className="movie-info">
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-        </data>
-        <data className="movie-info">
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </data>
-      </div>
-      <div className="right-section">
-        <div className="movie-image">
-          <img src={data.Poster} alt={data.Title} />
-        </div>
-      </div>
+        </>
+      )}
     </div>
-  )
+  );
 };
 
 export default MovieDetail;
--- src/features/movies/movieSlice.tsx
+++ src/features/movies/movieSlice.tsx
@@ -53,8 +53,8 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeDetails: (state) => {
+      state.details = {}
     },
   },
   // 'Builder callback' approach required for TypeScript to correctly infer types
@@ -84,7 +84,7 @@ const movieSlice = createSlice({
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeDetails } = movieSlice.actions;
 export const getAllMovies = (state: RootState) => state.movies.movies;
 export const getAllShows = (state: RootState) => state.movies.shows;
 export const getDetails = (state: RootState) => state.movies.details;
