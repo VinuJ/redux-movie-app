1951b58802f31e7c980718d7fd7136cbaf8a06e1
VinuJ <severematrixoverload@gmail.com>
2021-11-19 15:40:58 +0000

    Added dev to-do comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ import Home from "./components/Home/Home";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 
+
+/* 
+  TO-DO:
+  - Make MovieDetails responsive
+  - Add loader animation for normal search and loading movie details
+  - Nicer colours styling overall
+  - Fix home page margins for mobile screen width
+*/
+
 function App() {
   return (
     <div className="app">
